0007ed730e1f4a15f7f14cb6e314b3a23df2cf66
Anjani Sai Kumar Chatla <anjanisaikumar.chatla@asu.edu>
2017-08-03 06:37:12 +0000

    Final Commit

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,14 @@
 // Code Keypad Component Here
+
+import React from 'react';
+
+export default class Keypad extends React.Component {
+  keyup = () =>{
+    console.log ('Entering password...');
+  }
+  render(){
+    return (
+      <input onKeyUp={this.keyup} type="password"/>
+    );
+  }
+}
