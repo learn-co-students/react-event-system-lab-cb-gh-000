7c16dc018c89608ae54227434f0d8fd3397236ed
Ngoc Dong Pham <cpud1258@gmail.com>
2018-09-29 02:28:25 +0000

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,20 @@
 // Code EyesOnMe Component Here
+import React from 'react';
+export default class EyesOnMe extends React.Component {
+  constructor() {
+
+  }
+  onFocues=()=>{
+    console.log("Good!")
+  }
+  onBlur=()=>{
+    console.log("Hey! Eyes on me!")
+
+  }
+  render(){
+    return (<button
+      onFocus={ this.onFocues }
+           onBlur={ this.onBlur }
+      ></button>)
+  }
+}
