006918234a4f635cc0d032c23f66350a1f904161
Bianca Zanardi <bianca.zanardi@gmail.com>
2018-12-31 20:18:07 +0000

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,20 @@
-// Code EyesOnMe Component Here
+import React from 'react';
+
+class EyesOnMe extends React.Component {
+
+  focus = () => {
+    console.log('Good!');
+  }
+
+  blur = () => {
+    console.log('Hey! Eyes on me!');
+  }
+
+  render() {
+    return (
+      <button onFocus={this.focus} onBlur={this.blur}>For your eyes</button>
+    );
+  }
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,16 @@
-// Code Keypad Component Here
+import React from 'react';
+
+class Keypad extends React.Component {
+
+  typing = () => {
+    console.log('Entering password...');
+  }
+
+  render() {
+    return (
+      <input type="password" onKeyUp={this.typing} />
+    );
+  }
+}
+
+export default Keypad;
