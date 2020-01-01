947760d5fdaa27d68f2a2528137d528b78027905
Ri Xin Yang <rixin.yang.2001@gmail.com>
2020-01-01 23:34:24 +0000

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,15 @@
-// Code EyesOnMe Component Here
+import React from 'react'
+
+class EyesOnMe extends React.Component {
+
+  focused = () => {console.log("Good!");}
+  blured = () => {console.log("Hey! Eyes on me!");}
+
+  render() {
+    return (
+      <button onFocus={this.focused} onBlur={this.blured}>Button</button>
+    )
+  }
+}
+
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,14 @@
-// Code Keypad Component Here
+import React from 'react'
+
+class Keypad extends React.Component {
+
+  enteringPassword = () => {console.log("Entering password...");}
+
+  render() {
+    return (
+      <input type="password" onKeyUp={this.enteringPassword}/>
+    )
+  }
+}
+
+export default Keypad
