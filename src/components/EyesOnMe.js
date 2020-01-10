d06e61820a99637dc4e854b48904acab08f26b07
Recep Ekşi <portre_90@hotmail.com>
2020-01-10 12:33:13 +0000

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,16 @@
 // Code EyesOnMe Component Here
+import React from 'react';
+class EyesOnMe extends React.Component {
+  fonks=()=>{
+    console.log('Good!');
+  }
+  fonn=()=>{
+    console.log('Hey! Eyes on me!');
+  }
+  render() {
+    return (
+      <button onFocus={this.fonks} onBlur={this.fonn} />
+    )
+  }
+}
+export default EyesOnMe;
