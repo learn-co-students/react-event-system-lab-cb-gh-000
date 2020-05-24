26be033ace1b9c27a73236f0816b3a56b7bf93ac
Armintas Zadeika <az778@york.ac.uk>
2020-05-24 18:13:11 +0000

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,17 @@
 // Code EyesOnMe Component Here
+import React, { Component } from "react"
+
+class EyesOnMe extends Component{
+    blurry = ()=>{
+        console.log("Hey! Eyes on me!");
+    }
+    focusing = ()=>{
+        console.log("Good!");
+    }
+    render(){
+        return(
+            <button onFocus={this.focusing} onBlur={this.blurry}>Yo</button>
+        )
+    }
+}
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,14 @@
 // Code Keypad Component Here
+import React, { Component } from "react";
+
+class Keypad extends Component{
+    password = ()=>{
+        console.log("Entering password...");
+    }
+    render(){
+        return(
+            <input type="password" onKeyUp={this.password}></input>
+        )
+    }
+}
+export default Keypad;
